58a141578390a3f9d9f8e43447643625c96aa949
developer <dev@ip-10-227-25-167.us-west-2.compute.internal>
2013-11-02 06:53:46 +0000

    udpate yahoo

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -127,11 +127,10 @@ hello.init({
 
 })(hello);
 var YAHOO_CLI_ID = {
-	'dont-throw-layout':'dj0yJmk9cHZuRzlMOUkxMnB4JmQ9WVdrOVEzQmtha3RXTjJrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wOQ--'
-};
+	'dont-throw-layout':'dj0yJmk9cHZuRzlMOUkxMnB4JmQ9WVdrOVEzQmtha3RXTjJrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wOQ--'};
 hello.init({ 
 		yahoo   : YAHOO_CLI_ID
-	},{redirect_uri:'/aaa'});
+	},{redirect_uri:'test.html'});
 
 hello.on('auth.login', function(auth){
 		// call user information, for the given network
