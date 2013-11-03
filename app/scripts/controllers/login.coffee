47881163943c9d154832ca0744570cdf317a07f8
Justin <yuting1987@gmail.com>
2013-11-03 10:21:07 +0800

    Update

--- app/scripts/controllers/login.coffee
+++ app/scripts/controllers/login.coffee
@@ -2,4 +2,4 @@ dont.controller "LoginCtrl", ($scope,$location) ->
   $scope.items = []
   $scope.limit = 0
   $scope.href = () ->
-    $location.path("/#/choose")
+    location.href = '#/choose'
--- dist/scripts/controllers/login.js
+++ dist/scripts/controllers/login.js
@@ -2,7 +2,7 @@ dont.controller("LoginCtrl", function($scope, $location) {
   $scope.items = [];
   $scope.limit = 0;
   return $scope.href = function() {
-    return $location.path("/#/choose");
+    return location.href = '#/choose';
   };
 });
 
